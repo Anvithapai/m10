e31292c64468c6788d12c985fcedaa4b5944b5a2
pooja <poojamahale726@gmail.com>
2025-04-22 10:30:33 +0530

    first commit

---
+++ src/main/java/mavenp10/mavenp10/App.java
@@ -0,0 +1,13 @@
+package mavenp10.mavenp10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
